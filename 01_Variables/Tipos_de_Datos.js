d2f86e0b4ad63aefaf9d0270b09f0d3dcd330595
randymxd06 <randym0624@gmail.com>
2020-04-23 12:02:53 -0400

    Se agregaron los tipos de variables

---
+++ 01_Variables/Tipos_de_Datos.js
@@ -0,0 +1,30 @@
+// Texto = String
+let nombreCompleto = "Randy";
+let apellido = 'Martinez';
+let pais = `Republica Dominicana`;
+
+
+// Numero = Number
+let edad = 19;
+let año = 2020;
+let millón = 1000000;
+const PI = 3.14;
+let precioDollar = 54.8;
+
+// Logicos = Boolean;
+let tieneCarro = false;
+let poseePC = true;
+
+console.log(nombreCompleto);
+console.log(edad);
+console.log(tieneCarro);
+
+console.log(typeof nombreCompleto);
+console.log(typeof edad);
+console.log(typeof tieneCarro);
+
+let precio = 10;
+console.log(typeof precio);
+
+let resultado = precio + 15;
+console.log(resultado);
---
+++ 01_Variables/Variables.js
@@ -0,0 +1,17 @@
+let deuda = 1990;
+let nombre = "Santiago";
+let FechaNacimiento = "10/1/2000";
+
+console.log("tu deuda es de "+deuda);
+console.log("Bienvenido a tu pagina "+nombre);
+console.log("Naciste en la fecha "+FechaNacimiento);
+
+// Solo letras, numeros y $ _
+
+let apellido;
+let balance22;
+let $elemento;
+let _valor;
+
+// El primer caracter no puede ser un numero o otro caracter que no sea $ y _
+
