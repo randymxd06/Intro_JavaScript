504603b8fc0ae7543318c116c77d3d7051246052
randymxd06 <randym0624@gmail.com>
2020-04-30 10:49:58 -0400

    se agregó la carpeta Practica

---
+++ Practicas/Modificar_Objetos_Array.js
@@ -0,0 +1,24 @@
+let objeto = {
+    nombre: "Soy un objeto",
+    tipo: "Objeto"
+};
+
+console.log(objeto);
+
+objeto.nombre = "Randy";
+objeto.tipo = "Persona";
+objeto.edad = 25;
+objeto.tieneCarro = false;
+console.log(objeto);
+
+/*
+console.log(objeto);
+objeto.edad = 19;
+console.log(objeto);
+*/
+
+/*
+let objeto2 = {}
+objeto2.nombre = "Soy un objeto";
+objeto2.tipo = "Objeto";
+*/
