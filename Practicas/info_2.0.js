96358fc8302485547056100b92b937f44ff3d8c5
randymxd06 <randym0624@gmail.com>
2020-05-01 12:41:41 -0400

    Eliminé los if que estaban dentro de los while y puse una condicion para que la cantidad de dias no sea 0 o negativas ni mayores a 7

--- Practicas/info_2.0.js
+++ Practicas/info_2.0.js
@@ -1,32 +1,28 @@
 /*Declaro un Array para los dias de la semana y una
 variable para la cantidad de dias que quiero digitar*/
 let diasSemana = [];
-var cantDias = Number(prompt("Escriba la cantidad de días que usted desee digitar"));
+let cantDias = Number(prompt("Escriba la cantidad de días que usted desee digitar"));
 //Verifico que la cantidad de dias no sea Negativa o Cero//
-while(cantDias<=0){
-    if(cantDias<=0){
-        alert("Error, los días no pueden ser negativos o cero");
-        //Hasta que no digites una cantidad que sea mayor a 0 no saldrá del ciclo//
-        cantDias = Number(prompt("Escriba la cantidad de días que usted desee digitar"));
-    }
+while(cantDias < 1 || cantDias > 7){
+    alert("Error, los días no pueden ser negativos o cero tampoco puede ser mayor a 7");
+    //Hasta que no digites una cantidad que sea mayor a 0 no saldrá del ciclo//
+    cantDias = Number(prompt("Escriba la cantidad de días que usted desee digitar"));
 }
 //El for es para digitar los días de la semana//
-for(var i = 0 ; i < cantDias ; i++){
+for(let i = 0 ; i < cantDias ; i++){
     diasSemana[i] = prompt("Digite el día de la semana");
 }
 
 //Declaro un Array para almacenar los miembros de la familia//
-var miembroFamilia = [];
+let miembroFamilia = [];
 //El for es para ir pidiendo los familiares//
 for(i = 0 ; i <= 2 ; i++){
     miembroFamilia[i] = prompt("Digita el nombre de tu familiar");
     //En dado caso que dejes el campo vacío te dará un error//
     while(miembroFamilia[i] == ''){
-        if(miembroFamilia[i] == ''){
-            alert("Error, campo vacío porfavor digite de nuevo");
-            //No saldrá del ciclo hasta que el campo de texto tenga algo escrito//
-            miembroFamilia[i] = prompt("Digita el nombre de tu familiar");
-        }
+        alert("Error, campo vacío porfavor digite de nuevo");
+        //No saldrá del ciclo hasta que el campo de texto tenga algo escrito//
+        miembroFamilia[i] = prompt("Digita el nombre de tu familiar");
     }
 }
 
@@ -40,7 +36,7 @@ datosPersonales.nombre = prompt("Escribe tu nombre completo");
 datosPersonales.fechaNacimiento = prompt("Escribe tu fecha de nacimiento");
 datosPersonales.lugarNacimiento = prompt("Escribe tu lugar de nacimiento");
 datosPersonales.edad = Number(prompt("Escribe tu edad actual"));
-for(var i = 0 ; i <= 2 ; i++){
+for(let i = 0 ; i <= 2 ; i++){
     datosPersonales.comidaFavorita[i] = prompt("Escribe una comida favorita");
 }
 for(i = 0 ; i <= 4 ; i++){
@@ -57,7 +53,7 @@ let artista = {
 artista.nombre = prompt("Digita el nombre de tu artista favorito");
 artista.edad = Number(prompt("Digita la edad que crees que tiene tu artista favorito"));
 let cantCanciones = prompt(`Cuantas canciones favoritas de ${artista.nombre} tienes?`);
-for(var i = 0 ; i < cantCanciones ; i++){
+for(let i = 0 ; i < cantCanciones ; i++){
     artista.canciones[i] = prompt(`Escribe el nombre una cancion de ${artista.nombre}`);
 }
 artista.confesion = prompt("Si pudieras decirle algo a ese artista que le dirías?");
