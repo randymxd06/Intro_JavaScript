ed67e10da56e2f65c47ab965f3133117fc1c2b51
randymxd06 <randym0624@gmail.com>
2020-05-06 16:05:53 -0400

    Agregué el archivo ocultar.html y el archivo script.js

---
+++ Practicas/script.js
@@ -0,0 +1,10 @@
+let h1_text = document.querySelector(".cambio");
+let divText = document.getElementById("ocultar");
+let btnOcultar = document.querySelector("#boton");
+
+btnOcultar.addEventListener("dblclick", function(){
+    //divText.style.display = "none";
+    divText.classList = "d-none";
+    h1_text.style.color = "red";
+    h1_text.innerHTML = "<i>Haz hecho doble click</i>";
+});
